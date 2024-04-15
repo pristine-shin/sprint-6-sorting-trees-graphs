d7af4f964d2d83236761323f86065efabc01e40f
Pristine <prisvsyo@gmail.com>
2024-04-15 13:22:49 -0700

    completed ageSort and oddEvenSort

--- aa06-custom-sorts-part-time/custom-sorts.js
+++ aa06-custom-sorts-part-time/custom-sorts.js
@@ -1,21 +1,29 @@
 function ageSort(users) {
-  // Your code here 
+  // Your code here
+  return users.sort(function(a, b) {
+    return a.age - b.age
+  });
 }
 
 function oddEvenSort(arr) {
-  // Your code here 
+  // Your code here
+  return arr.sort(function (a, b) {
+    if (a % 2 === 1 && b % 2 === 0) return -1;
+    if (a % 2 === 0 && b % 2 === 1) return 1;
+    return a - b;
+  });
 }
 
 function validAnagrams(s, t) {
-  // Your code here 
+  // Your code here
 }
 
 function reverseBaseSort(arr) {
-  // Your code here 
+  // Your code here
 }
 
 function frequencySort(arr) {
-  // Your code here 
+  // Your code here
 }
 
 module.exports = [
