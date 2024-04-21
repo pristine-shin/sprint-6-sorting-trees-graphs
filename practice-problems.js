8835594af95572c3e3b6b1a5f4723c41dce54813
Pristine Shin <prisvsyo@gmail.com>
2024-04-21 12:47:28 -0700

    added to practice problems

--- practice-problems.js
+++ practice-problems.js
@@ -487,22 +487,22 @@ array at the end.*/
 // // returns the correct neighbors from an edge node
 // console.log(getNeighbors([2,0], matrix)) // returns [ [1,0], [3,0], [2,1] ]
 /******************************************************************************/
-class TreeNode {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
-}
-let a = new TreeNode('a');
-let b = new TreeNode('b');
-let c = new TreeNode('c');
-let d = new TreeNode('d');
-let e = new TreeNode('e');
-let f = new TreeNode('f');
+// class TreeNode {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+// let a = new TreeNode('a');
+// let b = new TreeNode('b');
+// let c = new TreeNode('c');
+// let d = new TreeNode('d');
+// let e = new TreeNode('e');
+// let f = new TreeNode('f');
 
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
