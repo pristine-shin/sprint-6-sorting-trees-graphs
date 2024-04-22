f9e8472841fcae3ef4a6d4bc45f8342e665107e3
Pristine <prisvsyo@gmail.com>
2024-04-22 11:53:13 -0700

    added island count to practice problems

--- practice-problems.js
+++ practice-problems.js
@@ -506,3 +506,38 @@ array at the end.*/
 // b.left = d;
 // b.right = e;
 // c.right = f;
+
+/******************************************************************************/
+//COUNTING ISLANDS FROM ALVIN'S CODE.
+const numIslands = function(grid) {
+  const visited = new Set();
+  let count = 0;
+
+  for (let r = 0; r < grid.length; r += 1) {
+      for (let c = 0; c < grid[0].length; c += 1) {
+          if (explore(grid, r, c, visited) === true) {
+              count += 1;
+          }
+      }
+  }
+  return count;
+};
+
+const explore = (grid, r, c, visited) => {
+  const rowInbounds = 0 <= r && r < grid.length;
+  const colInbounds = 0 <= c && c < grid[0].length;
+  if (!rowInbounds || !colInbounds) return false;
+
+  if (grid[r][c] === '0') return false;
+
+  const pos = r + ',' + c;
+  if (visited.has(pos)) return false;
+  visited.add(pos);
+
+  explore(grid, r - 1, c, visited);
+  explore(grid, r + 1, c, visited);
+  explore(grid, r, c - 1, visited);
+  explore(grid, r, c + 1, visited);
+
+  return true;
+}
