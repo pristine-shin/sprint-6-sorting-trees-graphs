db817056c744ee32c74802c2dae19302d3b08ec7
Pristine <prisvsyo@gmail.com>
2024-04-22 12:03:01 -0700

    added minimum Island size on practice problems

--- practice-problems.js
+++ practice-problems.js
@@ -541,3 +541,43 @@ const explore = (grid, r, c, visited) => {
 
   return true;
 }
+
+
+/******************************************************************************/
+//Finding size of smallest island FROM ALVIN'S CODE.
+const minimumIsland = (grid) => {
+  const visited = new Set();
+
+  let minSize = Infinity;
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[0].length; c++) {
+      const size = exploreSize(grid, r, c, visited);
+      if (size > 0 && size < minSize) {
+        minSize = size;
+      }
+    }
+  }
+
+  return minSize;
+};
+
+const exploreSize = (grid, r, c, visited) => {
+  const rowInbounds = 0 <= r && r < grid.length;
+  const colInbounds = 0 <= c && c < grid[0].length;
+  if (!rowInbounds || !colInbounds) return 0;
+
+  if (grid[r][c] === 'W') return 0;
+
+  const pos = r + ',' + c;
+  if (visited.has(pos)) return 0;
+  visited.add(pos);
+
+  let size = 1;
+  size += exploreSize(grid, r - 1, c, visited);
+  size += exploreSize(grid, r + 1, c, visited);
+  size += exploreSize(grid, r, c - 1, visited);
+  size += exploreSize(grid, r, c + 1, visited);
+
+  return size;
+
+};
